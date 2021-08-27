80c5594906ecf5f15b935b865488b5b34b8fefe1
Mateusz Niewiadomski <mateusz.niewiadomski@phoenix-rtos.com>
2021-08-27 15:48:39 +0200

    ports: micropython port for ia32 and ARM (imx106x)
     - MicroPython compiles and runs on imxrt106x target
     - patchfiles split into more atomic ones
     - patchfiles cleared
     - removed compiler specific linking. All links with gcc
     - unsetting CFLAGS and LDFLAGS in build.sh for less patched Makefiles
    
    JIRA: BES-145

---
+++ micropython/README.md
@@ -0,0 +1,31 @@
+# MicroPython port for Phoenix-RTOS 
+
+## Overview 
+
+This is a Micropython port for Phoenix-RTOS operating system. Currently it is working on following targets:
+
+ * `ia32-generic`
+ * `armv7m7-imxrt106x`
+
+Currently we are working to bring this port onto other targets so this list is going to be expanded.
+
+For building micropython we are using `unix` port provided by Micropython that is patched to suite Phoenix-RTOS system. All configuration and patches are stored inside `micropython-1.15-config/` directory and are implemented by `build.sh` script
+
+## File structure
+
+ * `build.sh` - building script that prepares environment and executes `make` related commands
+ * `micropython-1.15.tar.xz` - (may be missing before first build) archieve with MicroPython codebase 
+ * `micropython-1.15-config/`
+   * `files/`
+     * `001_mpconfigport.mk` - file containing Micropython port configuration of used third party libraries. As selection of those libraries can heavily impact the size of MicroPython binary size this configuration is stored as a whole file for easy management. During build it is copied into MicroPython codebase.
+   * `patches/`
+     * `01_libm_absent_libm.patch` - arithmetic functions missing in `libphoenix` substitution
+     * `02_libm_ifdefs.patch` - math related functions missing in `libphoenix` substitution in MicroPython libraries
+     * `03_mpy-cross_configs.patch` - minor cross compilator patch
+     * `04_mpy-cross_Makefile.patch` - minor patches for cross compilator Makefile 
+     * `05_os_configs.patch` - patches for internal port configurations
+     * `06_os_main.patch` - incorporating stack and heap size of MicroPython and addition of bsd `strlcat` and `strlcpy` functions
+     * `07_os_Makefile.patch`- disabling `Werror` compilation flag and addition of arithmetic functions absent in `libphoenix`
+     * `08_os_varia.patch` - missing defines corrections 
+     * `09_py_ifdefs.patch` - math related functions missing in `libphoenix` substitution in MicroPython code
+
--- micropython/build.sh
+++ micropython/build.sh
@@ -5,12 +5,13 @@ set -e
 UPYTH_VER="1.15"
 UPYTH="micropython-${UPYTH_VER}"
 
-b_log "Building micropython"
 PREFIX_UPYTH="${TOPDIR}/phoenix-rtos-ports/micropython"
 PREFIX_UPYTH_BUILD="${PREFIX_BUILD}/micropython"
 PREFIX_UPYTH_SRC=${PREFIX_UPYTH_BUILD}/${UPYTH}
-PREFIX_UPYTH_PATCHSET="${PREFIX_UPYTH}/${UPYTH}-patchset"
+PREFIX_UPYTH_CONFIG="${PREFIX_UPYTH}/${UPYTH}-config/"
 PREFIX_UPYTH_MARKERS="$PREFIX_UPYTH_BUILD/markers/"
+
+b_log "Building micropython"
 
 #
 # Download and unpack
@@ -21,24 +22,55 @@ mkdir -p "$PREFIX_UPYTH_BUILD" "$PREFIX_UPYTH_MARKERS"
 
 
 #
-# Apply patches
+# Apply patches and copy files
 #
-# FIXME: prefix patch files with numbers to ensure correct order
-for patchfile in "$PREFIX_UPYTH_PATCHSET"/*.patch; do
+for patchfile in "$PREFIX_UPYTH_CONFIG"/patches/*.patch; do
 	if [ ! -f "$PREFIX_UPYTH_MARKERS/$(basename "$patchfile").applied" ]; then
 		echo "applying patch: $patchfile"
-		patch -d "$PREFIX_UPYTH_SRC" -p1 < "$patchfile"   # FIXME: should be -p1
+		patch -d "$PREFIX_UPYTH_SRC" -p1 < "$patchfile"
 		touch "$PREFIX_UPYTH_MARKERS/$(basename "$patchfile").applied"
 	fi
 done
+cp -a "${PREFIX_UPYTH_CONFIG}/files/001_mpconfigport.mk" "${PREFIX_UPYTH_SRC}/ports/unix/mpconfigport.mk" && echo "Copied mpconfigport.mk!"
+
+
+#
+# Micropython internal use stack/heap size (not actual application stack/heap)
+# Values are to be overwritten in _targets/build.project.*
+#
+: "${UPYTH_STACKSZ=4096}"
+: "${UPYTH_HEAPSZ=16384}"
+
+
+#
+# Architecture specific flags/values set
+#
+if [ "${TARGET_FAMILY}" = "armv7m7" ]; then
+	STRIPEXP="--strip-unneeded"
+elif [ "${TARGET_FAMILY}" = "ia32" ]; then
+	STRIPEXP="--strip-all"
+else
+	b_log "Warning! Phoenix-RTOS for ${TARGET_FAMILY} does not support MicroPython compilation (yet!)"
+	b_log "The compilation attempt will start in 5 seconds..."
+	sleep 5
+fi
+# changing LDFLAGS from "ld" params format to "gcc" params - prefixing with -Wl, and changing spaces to colons
+LDFLAGS_VAR=$(echo " ${LDFLAGS}" | sed "s/\s/,/g" | sed "s/,-/ -Wl,-/g")
+
+export STRIPFLAGS_EXTRA="${STRIPEXP}"
+export PHOENIX_MATH_ABSENT="expm1 log1p asinh acosh atanh erf tgamma lgamma copysign __sin __cos __tan __signbit"
+export LDFLAGS_EXTRA="${CFLAGS} $LDFLAGS_VAR"
+export CFLAGS_EXTRA="${CFLAGS} -DUPYTH_STACKSZ=${UPYTH_STACKSZ} -DUPYTH_HEAPSZ=${UPYTH_HEAPSZ} "
+# clear original ld-format ldflags/cflags
+export LDFLAGS=""
+export CFLAGS=""
+
 
 #
 # Build and install micropython binary
 #
-# FIXME - remove clean
-(cd "${PREFIX_UPYTH_SRC}/mpy-cross" && make clean && make all BUILD="${PREFIX_UPYTH_BUILD}")
-(cd "${PREFIX_UPYTH_SRC}/ports/unix" && make clean && make all)
+(cd "${PREFIX_UPYTH_SRC}/mpy-cross" && make all BUILD="${PREFIX_UPYTH_BUILD}" CROSS_COMPILE="${CROSS}")
+(cd "${PREFIX_UPYTH_SRC}/ports/unix" && make all CROSS_COMPILE="${CROSS}")
 
-cp -a "${PREFIX_UPYTH_SRC}/ports/unix/micropython" "$PREFIX_PROG"
-"${CROSS}strip" -s "$PREFIX_PROG/micropython" -o "$PREFIX_PROG_STRIPPED/micropython"
-b_install "$PREFIX_PORTS_INSTALL/curl" /bin/
+cp -a "${PREFIX_UPYTH_SRC}/ports/unix/micropython" "$PREFIX_PROG_STRIPPED"
+b_install "$PREFIX_PORTS_INSTALL/micropython" /bin/
