649fe0b922891e24ab8dd0e8cf6c129e969b4286
Damian <damian.loewnau@phoenix-rtos.com>
2022-05-12 17:45:43 +0200

    mbedtls: download tarball with wget
    JIRA: PD-272

--- mbedtls/build.sh
+++ mbedtls/build.sh
@@ -6,36 +6,34 @@ MBEDTLS_VER="2.28.0"
 MBEDTLS="mbedtls-${MBEDTLS_VER}"
 
 PREFIX_MBEDTLS="${TOPDIR}/phoenix-rtos-ports/mbedtls"
-PREFIX_MBEDTLS_REPO="${PREFIX_MBEDTLS}/${MBEDTLS}"
 PREFIX_MBEDTLS_BUILD="${PREFIX_BUILD}/mbedtls"
+PREFIX_MBEDTLS_SRC="${PREFIX_MBEDTLS_BUILD}/${MBEDTLS}"
 PREFIX_MBEDTLS_PATCHES="${PREFIX_MBEDTLS}/patches"
 
 b_log "Building mbedtls"
 
 # Download and unpack
-if ! [ -d "${PREFIX_MBEDTLS_REPO}" ]; then
-	git clone -b "v${MBEDTLS_VER}" https://github.com/Mbed-TLS/mbedtls.git "${PREFIX_MBEDTLS_REPO}"
+mkdir -p "$PREFIX_MBEDTLS_BUILD"
+if ! [ -f "${PREFIX_MBEDTLS}/${MBEDTLS}.tar.gz" ]; then
+	wget https://github.com/Mbed-TLS/mbedtls/archive/v${MBEDTLS_VER}.tar.gz -O "${PREFIX_MBEDTLS}/${MBEDTLS}.tar.gz"
 fi
 
-rm -rf "${PREFIX_MBEDTLS_BUILD}"
+if ! [ -d "${PREFIX_MBEDTLS_SRC}" ]; then
+	tar xf "$PREFIX_MBEDTLS/${MBEDTLS}.tar.gz" -C "${PREFIX_MBEDTLS_BUILD}"
+fi
 
 # Apply patches
 for patchfile in "${PREFIX_MBEDTLS_PATCHES}"/*.patch; do
 	if ! [ -f "${PREFIX_MBEDTLS_PATCHES}/$(basename "$patchfile").applied" ]; then
 		echo "applying patch: $patchfile"
-		patch -d "${PREFIX_MBEDTLS}" -p0 -i "$patchfile" && touch "${PREFIX_MBEDTLS_PATCHES}/$(basename "$patchfile").applied"
+		patch -d "${PREFIX_MBEDTLS_BUILD}" -p0 -i "$patchfile" && touch "${PREFIX_MBEDTLS_PATCHES}/$(basename "$patchfile").applied"
 	fi
 done
-
-# Copy files to _build directory
-if ! [ -d "${PREFIX_MBEDTLS_BUILD}" ]; then
-	mkdir -p "${PREFIX_MBEDTLS_BUILD}"
-	cp -r "${PREFIX_MBEDTLS_REPO}" "${PREFIX_MBEDTLS_BUILD}"
-fi
 
 # Convert ldflags to format recognizable by gcc, for example -q -> -Wl,-q
 LDFLAGS=$(echo " ${LDFLAGS}" | sed "s/\s/,/g" | sed "s/,-/ -Wl,-/g")
 
-# Build
+# Flag that can be checked in makefiles
 export phoenix=1
+# Build
 (cd "${PREFIX_MBEDTLS_BUILD}/${MBEDTLS}" && make install no_test)
