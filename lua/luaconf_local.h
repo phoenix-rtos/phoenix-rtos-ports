7c6793d75d5b3e92e309a232c7d37ca893035aec
Daniel Sawka <daniel.sawka@phoenix-rtos.com>
2024-07-05 15:05:01 +0200

    lua: Make building more generic (build for any arch)
    Default version changed from 5.3.5 to 5.3.6.
    
    luaconf_local.h is required to be present in project's directory and
    can be used to override configuration values in e.g. luaconf.h
    
    Added port build options:
     - PORTS_LUA_CONFIG_DIR - path to a directory where luaconf_local.h is
       located. If not provided, the port's directory is used;
     - PORTS_LUA_VERSION - version to download (must be supported by
       respective patches);
     - PORTS_LUA_TESTS_VERSION - version of tests to download;
     - PORTS_LUA_BIN_CSTACK_SIZE - convenience option to set `-z stack-size`
       for lua binary built in this port;
     - PORTS_LUA_INSTALL_TESTS - download and install external Lua test
       scripts;
     - PORTS_LUA_SAFE - disable Lua features which may be used to crash
       the interpreter or perform side-effects in the operating system.
    
    JIRA: NIL-595

---
+++ lua/luaconf_local.h
@@ -0,0 +1,22 @@
+/* Examples of configuration options which may be set to override defaults
+used in luaconf.h. Some of these are not specified in luaconf.h, but in
+source files and can be overridden too (as long as they are ifdef'ed). */
+
+/*
+#define LUA_COMPAT_5_2
+#define LUA_COMPAT_5_1
+
+#define LUAI_MAXSTACK 4000
+#define LUA_MAXCAPTURES 32
+#define MAXCCALLS 5
+
+#define LUA_32BITS
+#define LUA_FLOAT_TYPE LUA_FLOAT_DOUBLE
+#define LUA_INT_TYPE LUA_INT_INT
+#define LUAL_BUFFERSIZE 512
+#define LUA_IDSIZE 30
+
+#define LUA_USE_APICHECK
+#define LUA_PATH_DEFAULT "/syspage/?.lua;" "/syspage/?/init.lua"
+#define LUA_CPATH_DEFAULT "/syspage/?.so"
+*/
