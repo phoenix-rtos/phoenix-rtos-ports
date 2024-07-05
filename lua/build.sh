b612039cf843b06028badae4b4a914a758326894
Daniel Sawka <daniel.sawka@phoenix-rtos.com>
2024-07-05 15:05:01 +0200

    lua: Make building more generic (build for any arch)
    Default version changed from 5.3.5 to 5.3.6.
    
    luaconf_local.h is required to be present in project's directory and
    can be used to override configuration values in e.g. luaconf.h
    
    Added port build options:
     - PORTS_LUA_CONFIG_DIR - path to a directory where luaconf_local.h is
       located. If not provided, the port's directory is used;
     - PORTS_LUA_VERSION - version to download (must be supported by
       respective patches);
     - PORTS_LUA_TESTS_VERSION - version of tests to download;
     - PORTS_LUA_BIN_CSTACK_SIZE - convenience option to set `-z stack-size`
       for lua binary built in this port;
     - PORTS_LUA_INSTALL_TESTS - download and install external Lua test
       scripts;
     - PORTS_LUA_SAFE - disable Lua features which may be used to crash
       the interpreter or perform side-effects in the operating system.
    
    JIRA: NIL-595

--- lua/build.sh
+++ lua/build.sh
@@ -2,31 +2,59 @@
 
 set -e
 
-LUA=lua-5.3.5
-PKG_URL="https://www.lua.org/ftp/${LUA}.tar.gz"
-PKG_MIRROR_URL="https://files.phoesys.com/ports/${LUA}.tar.gz"
+version="${PORTS_LUA_VERSION:-5.3.6}"
+archive_filename="lua-${version}.tar.gz"
+tests_version="${PORTS_LUA_TESTS_VERSION:-5.3.4}" # There is no 5.3.6 tag for tests
 
-PREFIX_LUA_SRC="${PREFIX_PORT_BUILD}/${LUA}"
+PREFIX_PORT_SRC="${PREFIX_PORT_BUILD}/${version}"
+PREFIX_PORT_TESTS="${PREFIX_PORT_BUILD}/${tests_version}-tests"
+: "${PORTS_LUA_CONFIG_DIR:=${PREFIX_PORT}}"
 
+b_port_download "https://www.lua.org/ftp/" "${archive_filename}"
 
-mkdir -p "$PREFIX_PORT_BUILD"
-if [ ! -f "$PREFIX_PORT/${LUA}.tar.gz" ]; then
-	if ! wget "$PKG_URL" -P "${PREFIX_PORT}" --no-check-certificate; then
-		wget "$PKG_MIRROR_URL" -P "${PREFIX_PORT}" --no-check-certificate
-	fi
+if [ ! -d "${PREFIX_PORT_SRC}" ]; then
+	echo "Extracting sources from ${archive_filename}"
+	mkdir -p "${PREFIX_PORT_SRC}"
+	tar -axf "${PREFIX_PORT}/${archive_filename}" --strip-components 1 -C "${PREFIX_PORT_SRC}"
 fi
-if [ ! -d "$PREFIX_LUA_SRC" ]; then
-	tar zxf "$PREFIX_PORT/${LUA}.tar.gz" -C "$PREFIX_PORT_BUILD"
-	cp "$PREFIX_PORT/Makefile" "$PREFIX_LUA_SRC/src/"
+
+b_port_apply_patches "${PREFIX_PORT_SRC}" "${version}"
+
+if [ "${PORTS_LUA_SAFE}" = "y" ]; then
+	b_port_apply_patches "${PREFIX_PORT_SRC}" "${version}/safe"
+fi
+
+# shellcheck disable=SC2206
+mycflags=(
+	${CFLAGS} # Split intended
+	"-I${PORTS_LUA_CONFIG_DIR}"
+)
+
+if [ -n "${PORTS_LUA_BIN_CSTACK_SIZE}" ]; then
+	LDFLAGS+=" -Wl,-z,stack-size=${PORTS_LUA_BIN_CSTACK_SIZE}"
 fi
 
 # FIXME: no out-of-tree building
-make -C "$PREFIX_LUA_SRC" posix
+make -C "${PREFIX_PORT_SRC}/src" MYCFLAGS="${mycflags[*]}" MYLDFLAGS="${LDFLAGS}"
+make -C "${PREFIX_PORT_SRC}" install INSTALL_TOP="${PREFIX_BUILD}"
 
-$STRIP -o "$PREFIX_PROG_STRIPPED/lua" "$PREFIX_LUA_SRC/src/lua"
-$STRIP -o "$PREFIX_PROG_STRIPPED/luac" "$PREFIX_LUA_SRC/src/luac"
-cp -a "$PREFIX_LUA_SRC/src/lua" "$PREFIX_PROG/lua"
-cp -a "$PREFIX_LUA_SRC/src/luac" "$PREFIX_PROG/luac"
+cp -a "${PORTS_LUA_CONFIG_DIR}/luaconf_local.h" "${PREFIX_H}/"
 
-b_install "$PREFIX_PORTS_INSTALL/lua" /bin
-b_install "$PREFIX_PORTS_INSTALL/luac" /bin
+$STRIP -o "${PREFIX_PROG_STRIPPED}/lua" "${PREFIX_PROG}/lua"
+$STRIP -o "${PREFIX_PROG_STRIPPED}/luac" "${PREFIX_PROG}/luac"
+
+b_install "${PREFIX_PORTS_INSTALL}/lua" /usr/bin
+b_install "${PREFIX_PORTS_INSTALL}/luac" /usr/bin
+
+if [ "${PORTS_LUA_INSTALL_TESTS}" = "y" ]; then
+	tests_filename="lua-${tests_version}-tests.tar.gz"
+	b_port_download "https://www.lua.org/tests/" "${tests_filename}"
+
+	if [ ! -d "${PREFIX_PORT_TESTS}" ]; then
+		echo "Extracting tests from ${tests_filename}"
+		mkdir -p "${PREFIX_PORT_TESTS}"
+		tar -axf "${PREFIX_PORT}/${tests_filename}" --strip-components 1 -C "${PREFIX_PORT_TESTS}"
+	fi
+
+	b_install "${PREFIX_PORT_TESTS}"/*.lua /usr/share/lua/tests
+fi
