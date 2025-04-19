4e29dfbee5e835e2aaef485bb74dbcc7814a3d6a
Glenn Strauss <gstrauss@gluelogic.com>
2025-04-19 00:07:56 -0400

    lighttpd: rename PREFIX_LIGHTTPD_SRC to PREFIX_PORT_SRC
    (consistency)

--- lighttpd/build.sh
+++ lighttpd/build.sh
@@ -6,7 +6,7 @@ LIGHTTPD="lighttpd-1.4.79"
 PKG_URL="https://download.lighttpd.net/lighttpd/releases-1.4.x/${LIGHTTPD}.tar.gz"
 PKG_MIRROR_URL="https://files.phoesys.com/ports/${LIGHTTPD}.tar.gz"
 
-PREFIX_LIGHTTPD_SRC="${PREFIX_PORT_BUILD}/${LIGHTTPD}"
+PREFIX_PORT_SRC="${PREFIX_PORT_BUILD}/${LIGHTTPD}"
 PREFIX_LIGHTTPD_MARKERS="$PREFIX_PORT_BUILD/markers/"
 
 PREFIX_OPENSSL=${PREFIX_BUILD}
@@ -21,7 +21,7 @@ if [ ! -f "$PREFIX_PORT/${LIGHTTPD}.tar.gz" ]; then
 		wget "$PKG_MIRROR_URL" -P "${PREFIX_PORT}" --no-check-certificate
 	fi
 fi
-[ -d "$PREFIX_LIGHTTPD_SRC" ] || tar zxf "$PREFIX_PORT/${LIGHTTPD}.tar.gz" -C "$PREFIX_PORT_BUILD"
+[ -d "$PREFIX_PORT_SRC" ] || tar zxf "$PREFIX_PORT/${LIGHTTPD}.tar.gz" -C "$PREFIX_PORT_BUILD"
 
 #
 # Apply patches
@@ -32,7 +32,7 @@ for patchfile in "${PREFIX_PORT}"/patches/*.patch; do
 	fi
 	if [ ! -f "$PREFIX_LIGHTTPD_MARKERS/$(basename "$patchfile").applied" ]; then
 		echo "applying patch: $patchfile"
-		patch -d "$PREFIX_LIGHTTPD_SRC" -p1 < "$patchfile"
+		patch -d "$PREFIX_PORT_SRC" -p1 < "$patchfile"
 		touch "$PREFIX_LIGHTTPD_MARKERS/$(basename "$patchfile").applied"
 	fi
 done
@@ -43,12 +43,12 @@ done
 if [ ! -f "$PREFIX_PORT_BUILD/config.h" ]; then
 	# FIXME: take into account commented-out modules
 	CONFIGFILE=$(find "${PREFIX_ROOTFS:?PREFIX_ROOTFS not set!}/etc" -name "lighttpd.conf")
-	grep mod_ "$CONFIGFILE" | cut -d'"' -f2 | xargs -L1 -I{} echo "PLUGIN_INIT({})" > "$PREFIX_LIGHTTPD_SRC"/src/plugin-static.h
+	grep mod_ "$CONFIGFILE" | cut -d'"' -f2 | xargs -L1 -I{} echo "PLUGIN_INIT({})" > "$PREFIX_PORT_SRC"/src/plugin-static.h
 
 	WITH_ZLIB="no" && [ "$PORTS_ZLIB" = "y" ] && WITH_ZLIB="yes"
 
-	( cd "$PREFIX_LIGHTTPD_SRC" && "./autogen.sh" )
-	( cd "$PREFIX_PORT_BUILD" && "$PREFIX_LIGHTTPD_SRC/configure" \
+	( cd "$PREFIX_PORT_SRC" && "./autogen.sh" )
+	( cd "$PREFIX_PORT_BUILD" && "$PREFIX_PORT_SRC/configure" \
 		CFLAGS="${CFLAGS} -DLIGHTTPD_STATIC" LDFLAGS="${LDFLAGS}" AR_FLAGS="-r" \
 		-C --host="$HOST" \
 		--prefix="$PREFIX_PORT_BUILD" --sbindir="$PREFIX_PROG" \
