4f55bb5bed022130edc6321c32f910fef0dc0c5b
Jan Wiśniewski <jan.wisniewski@phoenix-rtos.com>
2026-08-19 15:51:37 +0200

    micro-ecc: refactor port def and add cpe
    TASK: ISC-260

--- micro-ecc/port.def.sh
+++ micro-ecc/port.def.sh
@@ -5,15 +5,16 @@
 	ports_api=1
 
 	name="micro_ecc"
-	version="1.1.0+git.541b3a7"
-	desc="A small and fast ECDH and ECDSA implementation for 8-bit, 32-bit, and 64-bit processors."
-
 	commit="541b3a78026420a3e369c4c9281c396b5e531113"
-	git_source="https://github.com/kmackay/micro-ecc.git"
+	version_base="1.1.0"
+	version="${version_base}+git.${commit:0:8}"
+	desc="A small and fast ECDH and ECDSA implementation for 8-bit, 32-bit, and 64-bit processors."
+	cpe23="cpe:2.3:a:micro-ecc_project:micro-ecc:${version_base}:*:*:*:*:*:*:*"
+
 	source="https://github.com/kmackay/micro-ecc/archive"
 	archive_filename=("micro-ecc-${commit}.tar.gz" "${commit}.tar.gz")
 
-	src_path="micro-ecc-541b3a78026420a3e369c4c9281c396b5e531113"
+	src_path="micro-ecc-${commit}"
 
 	size="90435"
 	sha256="48b36c6ae7401dd27f5b478764cad8e42ca601e4e81f9984d07ec1c7f1251329"
@@ -24,6 +25,8 @@
 	depends=""
 
 	supports="phoenix>=3.3"
+
+	unset -v version_base commit
 }
 
 p_prepare() {
@@ -32,17 +35,18 @@ p_prepare() {
 }
 
 p_build() {
-	# -DuECC_OPTIMIZATION_LEVEL=2 ensures assembly optimizations are not used by default
-	# -DuECC_POSIX=1 ensures dev/urandom is used as RNG by default
-	local uECC_flags="-DuECC_OPTIMIZATION_LEVEL=2 -DuECC_POSIX=1"
-
-	# shellcheck disable=SC2086
-	(cd "${PREFIX_PORT_WORKDIR}" &&
-		"${CROSS}gcc" ${CFLAGS} ${uECC_flags} -I"${PREFIX_H}" -c uECC.c -o uECC.o)
-
-	(cd "${PREFIX_PORT_WORKDIR}" &&
-		"${CROSS}ar" rcs libuecc.a uECC.o)
-
-	cp -a "${PREFIX_PORT_WORKDIR}/libuecc.a" "${PREFIX_A}/"
-	cp -a "${PREFIX_PORT_WORKDIR}/uECC.h" "${PREFIX_H}/"
+	local -a cflagsArray
+	IFS=" " read -r -a cflagsArray <<<"${CFLAGS:?}"
+	cflagsArray+=(
+		"-DuECC_OPTIMIZATION_LEVEL=2" # highest optimization level without assembly code
+		"-DuECC_POSIX=1"              # use /dev/urandom as RNG source
+	)
+	(
+		set -e
+		cd "${PREFIX_PORT_WORKDIR:?}"
+		"${CC:?}" "${cflagsArray[@]}" -I"${PREFIX_H:?}" -c uECC.c -o uECC.o
+		"${AR:?}" rcs libuecc.a uECC.o
+		install -p -t "${PREFIX_A:?}" libuecc.a
+		install -p -t "${PREFIX_H:?}" uECC.h
+	)
 }
